90adbe74e94d46890126f590f1e3d4952626100d
noahhonsaker <noahhonsaker@gmail.com>
2026-04-21 11:28:52 -0400

    Update Program.cs

--- FinalProject/Program.cs
+++ FinalProject/Program.cs
@@ -1,25 +1,45 @@
-var builder = WebApplication.CreateBuilder(args);
+using FinalProject.Models;
+using Microsoft.EntityFrameworkCore;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace FinalProject.Data
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public class DefultContext : DbContext
+    {
+        public DefultContext(DbContextOptions<DefultContext> options) : base(options)
+        {
+        }
+
+        public DbSet<TeamMember> TeamMembers { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<TeamMember>().HasData(
+                new TeamMember
+                {
+                    Id = 1,
+                    FullName = "Noah Honsaker",
+                    Birthdate = new DateTime(2000, 1, 2),
+                    CollegeProgram = "IT",
+                    YearInProgram = "Sophmore"
+                },
+                new TeamMember
+                {
+                    Id = 2,
+                    FullName = "Ellie Bennings",
+                    Birthdate = new DateTime(2001, 3, 4),
+                    CollegeProgram = "Digital Media",
+                    YearInProgram = "Sophmore"
+                },
+                new TeamMember
+                {
+                    Id = 3,
+                    FullName = "Micheal Jones",
+                    Birthdate = new DateTime(2000, 5, 6),
+                    CollegeProgram = "IT",
+                    YearInProgram = "Sophmore"
+                }
+            );
+        }
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
