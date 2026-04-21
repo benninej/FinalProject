f937524f98cc08f56f511ebe1dfb3a4eade9af90
noahhonsaker <noahhonsaker@gmail.com>
2026-04-21 11:30:19 -0400

    Update TeamMembers.cs

--- FinalProject/Models/TeamMembers.cs
+++ FinalProject/Models/TeamMembers.cs
@@ -1,12 +1,20 @@
-﻿namespace FinalProject.Models
-{
-    public class TeamMembers
-    {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Brithday { get; set; }
-        public string Program { get; set; }
-        public int Year { get; set; }
+using System;
+using System.ComponentModel.DataAnnotations;
 
+namespace FinalProject.Models
+{
+    public class TeamMember
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string FullName { get; set; } = string.Empty;
+
+        public DateTime Birthdate { get; set; }
+
+        public string CollegeProgram { get; set; } = string.Empty;
+
+        public string YearInProgram { get; set; } = string.Empty;
     }
 }
