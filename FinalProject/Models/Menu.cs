ef3d76c37923c178aee3a31d2ceec8b40cbb2dfc
noahhonsaker <noahhonsaker@gmail.com>
2026-04-23 11:52:33 -0400

    added menu

---
+++ FinalProject/Models/Menu.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FinalProject.Models
+{
+    public class Menu
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        public int FoodId { get; set; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        public string Category { get; set; } = string.Empty;
+
+        public bool IsVegetarian { get; set; }
+    }
+}
