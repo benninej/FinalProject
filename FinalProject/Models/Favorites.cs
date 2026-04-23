0d7da6e29cfe365e26d6f5f0b39695a86e2cc48e
noahhonsaker <noahhonsaker@gmail.com>
2026-04-23 11:51:46 -0400

    added favorites
    added favorites for michael

---
+++ FinalProject/Models/Favorites.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FinalProject.Models
+{
+    public class Favorites
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        public string Food { get; set; } = string.Empty;
+        public string Music { get; set; } = string.Empty;
+        public string Hobby { get; set; } = string.Empty;
+        public string Animal { get; set; } = string.Empty;
+    }
+}
